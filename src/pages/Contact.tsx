c3daf5dc01a8bc31b9180106317493e96aad30d0
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-05-23 15:23:00 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -293,7 +293,7 @@ const Contact = () => {
 
   <div className="relative w-full h-[450px] overflow-hidden shadow-2xl rounded-none md:rounded-lg mx-auto max-w-7xl mt-10">
     <iframe 
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.868334149075!2d76.31281007569768!3d10.027722372555747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d5bc4f162f3%3A0x8f0b00fda4f74a01!2sCVM%20Arcade!5e0!3m2!1sen!2sin!4v1747977734786!5m2!1sen!2sin"
+      src="https://www.google.com/maps/d/embed?mid=1P9GHdOjApQAyc4RCcuOEXbXNjpzfy_Y&ehbc=2E312F"
       width="100%" 
       height="100%" 
       style={{ border: 0 }} 
